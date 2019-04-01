bbc8eb96ecc7d87db1366be95982a5dd4495eafc
ljt0815 <ljt0815@naver.com>
2019-04-01 21:40:57 +0900

    Updates

---
+++ precache-manifest.1b2ff927b480a510fc357a386baefcc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1363fa219169051e977d",
+    "url": "/web1/static/css/main.32a516a6.chunk.css"
+  },
+  {
+    "revision": "1363fa219169051e977d",
+    "url": "/web1/static/js/main.e7eec5d3.chunk.js"
+  },
+  {
+    "revision": "6bcb2d593014ef195486",
+    "url": "/web1/static/js/runtime~main.da9391b8.js"
+  },
+  {
+    "revision": "4ff4439962131c47cd59",
+    "url": "/web1/static/js/2.ed6b2ec3.chunk.js"
+  },
+  {
+    "revision": "8f4670d569c123ef90323068b539c769",
+    "url": "/web1/index.html"
+  }
+];
